0accb21e02e643aca62d18293336949a43a3a28e
Daniel Bauke <daniel.bauke@zalando.de>
2016-08-29 17:01:40 +0200

    #167: Come back to simpler xcpretty call

--- lib/calypso/xcode.rb
+++ lib/calypso/xcode.rb
@@ -79,7 +79,7 @@ module Calypso
       if env_skip_xcpretty?
         cmd
       else
-        "#{cmd} | xcpretty -f #{`xcpretty-travis-profiler-formatter`} -c && exit ${PIPESTATUS[0]}"
+        "set -o pipefail && #{cmd} | xcpretty"
       end
     end
 
