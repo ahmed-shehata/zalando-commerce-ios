f31413500f40b2fd6ef1b64c47c54a944e29dac3
Daniel Bauke <daniel.bauke@zalando.de>
2016-08-31 13:49:30 +0200

    #177: Temporary find to check project and VM structure

--- AtlasDemo/buddybuild_postbuild.sh
+++ AtlasDemo/buddybuild_postbuild.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-cd ..
+# cd ..
 bash <(curl -s https://codecov.io/bash) -J AtlasSDK -J AtlasUI
+
+echo "=== Project structure"
+find .. -type d
+
+echo "=== VM Structure"
+find / -type d
 
