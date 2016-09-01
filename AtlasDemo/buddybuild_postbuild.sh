ac682c74e6ed96040a5b572da7a6d88a072a5f18
Daniel Bauke <daniel.bauke@zalando.de>
2016-09-01 13:51:03 +0200

    #177: Added forgotten “cd ..” in buddybuild_postbuild.sh

--- AtlasDemo/buddybuild_postbuild.sh
+++ AtlasDemo/buddybuild_postbuild.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+cd ..
 bash <(curl -s https://codecov.io/bash) -J AtlasSDK -J AtlasUI -D /tmp/sandbox/${BUDDYBUILD_APP_ID}/bbbuild
 
 # find / -type f -name '*.profdata' 2> /dev/null
