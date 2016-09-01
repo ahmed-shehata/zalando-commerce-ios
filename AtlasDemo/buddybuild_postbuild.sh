582f7961516e254594ec578e61924753941e66c6
Daniel Bauke <daniel.bauke@zalando.de>
2016-09-01 17:11:41 +0200

    #177: Updated path to coverage reports in buddybuild_postbuild.sh

--- AtlasDemo/buddybuild_postbuild.sh
+++ AtlasDemo/buddybuild_postbuild.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 cd ..
-bash <(curl -s https://codecov.io/bash) -J AtlasSDK -J AtlasUI -D /tmp/sandbox/${BUDDYBUILD_APP_ID}/bbbuild
+bash <(curl -s https://codecov.io/bash) -J AtlasSDK -J AtlasUI -D /private/tmp/sandbox/${BUDDYBUILD_APP_ID}/bbtest
 
 # find / -type f -name '*.profdata' 2> /dev/null
