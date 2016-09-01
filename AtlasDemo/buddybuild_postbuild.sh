00504feeb1860f127892cac81e227ca00313b9f6
Daniel Bauke <daniel.bauke@zalando.de>
2016-09-01 17:41:00 +0200

    #177: Add more options to codecov uploader

--- AtlasDemo/buddybuild_postbuild.sh
+++ AtlasDemo/buddybuild_postbuild.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 cd ..
-bash <(curl -s https://codecov.io/bash) -J AtlasSDK -J AtlasUI -D /private/tmp/sandbox/${BUDDYBUILD_APP_ID}/bbtest -Z -K
+bash <(curl -s https://codecov.io/bash) -J AtlasSDK -J AtlasUI -D /private/tmp/sandbox/${BUDDYBUILD_APP_ID}/bbtest -Z -K -B ${BUDDYBUILD_BRANCH} -b ${BUDDYBUILD_BUILD_NUMBER}
 
 # find / -type f -name '*.profdata' 2> /dev/null
