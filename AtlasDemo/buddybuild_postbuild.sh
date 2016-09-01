f99bae627545a6007975fd30785eb874164c74bd
Daniel Bauke <daniel.bauke@zalando.de>
2016-09-01 12:22:07 +0200

    #177: Added back unit test targets to AtlasDemo schema

--- AtlasDemo/buddybuild_postbuild.sh
+++ AtlasDemo/buddybuild_postbuild.sh
@@ -3,4 +3,4 @@
 cd ..
 # bash <(curl -s https://codecov.io/bash) -J AtlasSDK -J AtlasUI -D /tmp/sandbox/${BUDDYBUILD_APP_ID}/bbbuild
 
-find / -type f -name '*.profdata'
+find / -type f -name '*.profdata' 2> /dev/null
