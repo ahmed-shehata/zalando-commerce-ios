860e387a647b9961f438daf5449ffdd3ddb91fb7
Ahmed Shehata <ahmed.shehata@zalando.de>
2016-09-02 12:10:56 +0200

    Fixes on PR (Edgeinsets + lines)

--- AtlasUI/AtlasUI/AddressRowViewCell.swift
+++ AtlasUI/AtlasUI/AddressRowViewCell.swift
@@ -4,6 +4,7 @@
 
 import Foundation
 import AtlasSDK
+
 final class AddressRowViewCell: UITableViewCell {
 
     var address: Address! {
@@ -25,13 +26,6 @@ final class AddressRowViewCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
 
-    private struct Metrics {
-        static let topPadding: CGFloat = 10
-        static let bottomPadding: CGFloat = 10
-        static let leadingPadding: CGFloat = 15
-        static let trailingPadding: CGFloat = 15
-    }
-
     private func setupViews() {
         contentView.backgroundColor = .clearColor()
 
@@ -39,31 +33,20 @@ final class AddressRowViewCell: UITableViewCell {
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         titleLabel.numberOfLines = 0
         contentView.addSubview(titleLabel)
-        titleLabel.topAnchor.constraintEqualToAnchor(contentView.topAnchor, constant: Metrics.topPadding).active = true
-        titleLabel.leadingAnchor.constraintEqualToAnchor(contentView.leadingAnchor, constant: Metrics.leadingPadding).active = true
-        titleLabel.trailingAnchor.constraintEqualToAnchor(contentView.trailingAnchor, constant: -Metrics.trailingPadding).active = true
-
+        titleLabel.layoutMargins = UIEdgeInsets(top: 10, left: 15, bottom: 10, right: 15)
         streetLabel.translatesAutoresizingMaskIntoConstraints = false
         streetLabel.numberOfLines = 0
         contentView.addSubview(streetLabel)
         streetLabel.topAnchor.constraintEqualToAnchor(titleLabel.bottomAnchor).active = true
         streetLabel.leadingAnchor.constraintEqualToAnchor(titleLabel.leadingAnchor).active = true
         streetLabel.trailingAnchor.constraintEqualToAnchor(titleLabel.trailingAnchor).active = true
-        streetLabel.bottomAnchor.constraintEqualToAnchor(contentView.bottomAnchor, constant: -Metrics.bottomPadding).active = true
+        streetLabel.bottomAnchor.constraintEqualToAnchor(contentView.bottomAnchor, constant: -10).active = true
     }
 
     private func updateViews() {
         titleLabel.text = address.firstName
-
-        var lines = [String]()
-
         if let street = address.street {
-            lines.append(street)
+            streetLabel.text = [street, "\(address.zip) \(address.city)", address.countryCode].flatMap { $0 }.joinWithSeparator("\n")
         }
-
-        lines.append("\(address.zip) \(address.city)")
-        lines.append(address.countryCode)
-
-        streetLabel.text = lines.joinWithSeparator("\n")
     }
 }
