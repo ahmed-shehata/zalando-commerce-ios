1b405e00dfce35d662353282838891e1d19e6971
hani-ibrahim <hani.eloksh@zalando.de>
2016-09-23 18:07:36 +0200

    #309: returning the forms patterns

--- AtlasUI/AtlasUI/AddressFormType.swift
+++ AtlasUI/AtlasUI/AddressFormType.swift
@@ -106,11 +106,11 @@ enum AddressFormField: String {
         case .FirstName:
             return [.Required,
                     .MaxLength(maxLength: 50),
-                    .Pattern(pattern: "^[' \\w-]+$")]
+                    .Pattern(pattern: "^["+FormValidator.anyCharacterPattern+"]'?[- "+FormValidator.anyCharacterPattern+"ß]+$")]
         case .LastName:
             return [.Required,
                     .MaxLength(maxLength: 50),
-                    .Pattern(pattern: "^[' \\w-]+$")]
+                    .Pattern(pattern: "^["+FormValidator.anyCharacterPattern+"]'?[- "+FormValidator.anyCharacterPattern+"ß]+$")]
         case .Street:
             return [.Required,
                     .MaxLength(maxLength: 50),
@@ -132,7 +132,7 @@ enum AddressFormField: String {
         case .City:
             return [.Required,
                     .MaxLength(maxLength: 50),
-                    .Pattern(pattern: "^[,;()'0-9 \\w-]+$")]
+                    .Pattern(pattern: "^["+FormValidator.anyCharacterPattern+"]'?[-,;()' 0-9"+FormValidator.anyCharacterPattern+"ß]+$")]
         case .Country:
             return [.Required]
         }
