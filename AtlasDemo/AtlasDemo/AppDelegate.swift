6deeb8d40f9bec4dbd146f7afdcae6ec9cb84c0a
Gleb Galkin <gleb.galkin@zalando.de>
2016-09-27 14:49:14 +0200

    #337: screenshot functionality added for bug bashing

--- AtlasDemo/AtlasDemo/AppDelegate.swift
+++ AtlasDemo/AtlasDemo/AppDelegate.swift
@@ -3,16 +3,51 @@
 //
 
 import UIKit
+import MessageUI
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, MFMailComposeViewControllerDelegate {
 
     var window: UIWindow?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         BuddyBuildSDK.setup()
         AppSetup.configure()
+
+        NSNotificationCenter.defaultCenter().addObserverForName(UIApplicationUserDidTakeScreenshotNotification,
+                                                                object: nil,
+                                                                queue: NSOperationQueue.mainQueue()) { notification in
+            let messageController = MFMailComposeViewController()
+            messageController.setSubject("Bug Bashing")
+            messageController.mailComposeDelegate = self
+
+            if let windowRect = self.window?.bounds {
+                UIGraphicsBeginImageContext(windowRect.size)
+
+                if let ctx = UIGraphicsGetCurrentContext() {
+                    CGContextFillRect(ctx, windowRect)
+                    self.window?.layer.renderInContext(ctx)
+                    let viewimage = UIGraphicsGetImageFromCurrentImageContext()
+                    let imageData = UIImageJPEGRepresentation(viewimage, 1.0)
+
+                    if let data = imageData {
+                        messageController.addAttachmentData(data, mimeType: "image/jpeg", fileName: "Bug")
+                    }
+                }
+
+                UIGraphicsEndImageContext()
+            }
+
+            let rootVC = UIApplication.sharedApplication().keyWindow?.rootViewController
+            rootVC?.dismissViewControllerAnimated(true, completion: nil)
+            rootVC?.presentViewController(messageController, animated: true, completion: nil)
+        }
+
         return true
+    }
+
+    func mailComposeController(controller: MFMailComposeViewController, didFinishWithResult result: MFMailComposeResult, error: NSError?) {
+        UIApplication.sharedApplication().keyWindow?.rootViewController?.dismissViewControllerAnimated(true, completion: nil)
     }
 
 }
