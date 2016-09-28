3bf1a422eab88b7ee5cae462d09e3469204d8ea5
Gleb Galkin <gleb.galkin@zalando.de>
2016-09-28 16:28:11 +0200

    #347: device info added to Bug Screenshot

--- AtlasDemo/AtlasDemo/AppDelegateBugBashingExtension.swift
+++ AtlasDemo/AtlasDemo/AppDelegateBugBashingExtension.swift
@@ -2,29 +2,19 @@ import UIKit
 import MessageUI
 
 extension AppDelegate: MFMailComposeViewControllerDelegate {
+
     func setupBugScreenshot() {
         NSNotificationCenter.defaultCenter().addObserverForName(UIApplicationUserDidTakeScreenshotNotification,
                                                                 object: nil,
                                                                 queue: NSOperationQueue.mainQueue()) { notification in
             let messageController = MFMailComposeViewController()
             messageController.setSubject("Bug Bashing")
+            messageController.setToRecipients(["support+cffaf505af80456e87bab4511fd5187d@feedback.hockeyapp.net"])
+            messageController.setMessageBody(self.getDeviceInfo(), isHTML: false)
             messageController.mailComposeDelegate = self
 
-            if let windowRect = self.window?.bounds {
-                UIGraphicsBeginImageContext(windowRect.size)
-
-                if let ctx = UIGraphicsGetCurrentContext() {
-                    CGContextFillRect(ctx, windowRect)
-                    self.window?.layer.renderInContext(ctx)
-                    let viewimage = UIGraphicsGetImageFromCurrentImageContext()
-                    let imageData = UIImageJPEGRepresentation(viewimage, 1.0)
-
-                    if let data = imageData {
-                        messageController.addAttachmentData(data, mimeType: "image/jpeg", fileName: "Bug")
-                    }
-                }
-
-                UIGraphicsEndImageContext()
+            if let data = self.getAttachment() {
+                messageController.addAttachmentData(data, mimeType: "image/jpeg", fileName: "Bug")
             }
 
             let rootVC = UIApplication.sharedApplication().keyWindow?.rootViewController
@@ -48,6 +38,27 @@ extension AppDelegate: MFMailComposeViewControllerDelegate {
         }
     }
 
+    private func getDeviceInfo() -> String {
+        let device = UIDevice.currentDevice()
+        return "\n\n\n-------------\n\(device.modelName)\n\(device.systemName) \(device.systemVersion)"
+    }
+
+    private func getAttachment() -> NSData? {
+        guard let windowRect = self.window?.bounds else { return nil }
+
+        UIGraphicsBeginImageContext(windowRect.size)
+
+        guard let ctx = UIGraphicsGetCurrentContext() else { return nil }
+        CGContextFillRect(ctx, windowRect)
+        self.window?.layer.renderInContext(ctx)
+        let viewImage = UIGraphicsGetImageFromCurrentImageContext()
+        let imageData = UIImageJPEGRepresentation(viewImage, 1.0)
+
+        UIGraphicsEndImageContext()
+
+        return imageData
+    }
+
     private func showAlert(text: String) {
         let alert = UIAlertController(title: "Bug Bashing",
                                       message: text,
@@ -59,4 +70,5 @@ extension AppDelegate: MFMailComposeViewControllerDelegate {
     private func dismissEmail() {
         UIApplication.sharedApplication().keyWindow?.rootViewController?.dismissViewControllerAnimated(true, completion: nil)
     }
+
 }
