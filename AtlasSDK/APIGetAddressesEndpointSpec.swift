ac0d43cc4aedfdadc420938edd585c9c888a0a71
Daniel Bauke <daniel.bauke@zalando.de>
2016-09-30 15:11:05 +0200

    #93: Automatic cleanup

--- AtlasSDK/APIGetAddressesEndpointSpec.swift
+++ AtlasSDK/APIGetAddressesEndpointSpec.swift
@@ -1,5 +1,5 @@
 //
-//   Copyright © 2016 Zalando SE. All rights reserved.
+//  Copyright © 2016 Zalando SE. All rights reserved.
 //
 
 import Foundation
@@ -28,4 +28,3 @@ class APISalesChannelSpec: QuickSpec {
     }
 
 }
-
