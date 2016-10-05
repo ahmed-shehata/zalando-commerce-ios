9a9d63e65dd5f04efc3e616dc4473bc732b52be9
Daniel Bauke <daniel.bauke@zalando.de>
2016-10-05 11:00:02 +0200

    Fix for Localizer tests

--- AtlasUI/AtlasUITests/LocalizationSpec.swift
+++ AtlasUI/AtlasUITests/LocalizationSpec.swift
@@ -13,19 +13,19 @@ class LocalizationSpec: QuickSpec {
     override func spec() {
         describe("Date extensions") {
             it("should format euro price after the number") {
-                let deutschland = try! Localizer(localeIdentifier: "de_DE", localizedStringsBundle: NSBundle.mainBundle()) // swiftlint:disable:this force_try
+                let deutschland = try! Localizer(localeIdentifier: "de_DE") // swiftlint:disable:this force_try
                 let price = deutschland.price(10)
                 expect(price).to(equal("10,00 €"))
             }
 
             it("should format euro price before the number") {
-                let france = try! Localizer(localeIdentifier: "en_FR", localizedStringsBundle: NSBundle.mainBundle()) // swiftlint:disable:this force_try
+                let france = try! Localizer(localeIdentifier: "en_FR") // swiftlint:disable:this force_try
                 let price = france.price(10)
                 expect(price).to(equal("€10,00"))
             }
 
             it("should format pound price") {
-                let brexit = try! Localizer(localeIdentifier: "en_UK", localizedStringsBundle: NSBundle.mainBundle()) // swiftlint:disable:this force_try
+                let brexit = try! Localizer(localeIdentifier: "en_UK") // swiftlint:disable:this force_try
                 let price = brexit.price(10)
                 expect(price).to(equal("£10.00"))
             }
