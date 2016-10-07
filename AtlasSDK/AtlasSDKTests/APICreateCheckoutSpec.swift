24e709f3acdfda9181d128cfa633fb045b4c77f2
hani-ibrahim <hani.eloksh@zalando.de>
2016-10-07 19:17:51 +0200

    #331: fix error

--- AtlasSDK/AtlasSDKTests/APICreateCheckoutSpec.swift
+++ AtlasSDK/AtlasSDKTests/APICreateCheckoutSpec.swift
@@ -45,8 +45,8 @@ class APICreateCheckoutSpec: APIClientBaseSpec {
                         switch result {
                         case .failure(let error):
                             fail(String(error))
-                        case .success(let checkoutCart):
-                            expect(checkoutCart.checkout.id).to(equal(self.checkoutId))
+                        case .success(let checkout):
+                            expect(checkout.id).to(equal(self.checkoutId))
                         }
                         done()
                     }
