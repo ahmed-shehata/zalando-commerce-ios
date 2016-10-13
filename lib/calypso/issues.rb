d9a88825822453333e256b1126c39511e2fc71f2
Daniel Bauke <daniel.bauke@zalando.de>
2016-10-13 18:21:23 +0200

    #476: Fetch cards from github projects API [wip]
    Added a call to github API in calypso to get cards API url

--- lib/calypso/issues.rb
+++ lib/calypso/issues.rb
@@ -16,16 +16,60 @@ module Calypso
       end
     end
 
+    desc 'release_notes <project_name> <column_name>', 'Shows issues in given project / column'
+    def release_notes(project_name, column_name)
+      cards_url(project_name, column_name)
+    end
+
     private
 
-    def github_url(labels, state, owner, repo)
-      "https://api.github.com/repos/#{owner}/#{repo}/issues?per_page=1000&labels=#{labels}&state=#{state}"
+    def repos_url(owner, repo, endpoint)
+      "https://api.github.com/repos/#{owner}/#{repo}/#{endpoint}"
+    end
+
+    def issues_url(labels, state, owner, repo)
+      repos_url(owner, repo, 'issues') + "?per_page=1000&labels=#{labels}&state=#{state}"
+    end
+
+    def projects_url(owner, repo)
+      repos_url(owner, repo, 'projects')
+    end
+
+    def columns_url(project_id, owner, repo)
+      repos_url(owner, repo, "projects/#{project_id}/columns")
+    end
+
+    def cards_url(project_name, column_name)
+      project = project(project_name)
+      column = column(project, column_name)
+
+      puts repos_url(env_owner, env_repo, "project/columns/#{column['id']}/cards")
     end
 
     def issues(labels, state)
-      url = github_url(labels, state, env_owner, env_repo)
-      response = HTTParty.get(url)
-      response.parsed_response.select { |issue| issue['pull_request'].nil? }
+      url = issues_url(labels, state, env_owner, env_repo)
+      fetch(url).select { |issue| issue['pull_request'].nil? }
+    end
+
+    def project(name)
+      projects = fetch_projects(projects_url(env_owner, env_repo))
+      projects.select { |p| p['name'] == name }.first
+    end
+
+    def column(project, name)
+      columns = fetch_projects(columns_url(project['number'], env_owner, env_repo))
+      columns.select { |c| c['name'] == name }.first
+    end
+
+    def fetch_projects(url)
+      headers = { 'Accept' => 'application/vnd.github.inertia-preview+json', 'User-Agent' => 'calypso.rb' }
+      fetch(url, headers)
+    end
+
+    def fetch(url, headers = nil)
+      puts "Fetching #{url}..."
+      response = HTTParty.get(url, headers: headers)
+      response.parsed_response
     end
 
     def format_issue(issue)
