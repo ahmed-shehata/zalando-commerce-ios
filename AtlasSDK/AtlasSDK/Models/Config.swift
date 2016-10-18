643bf0d84e7fc0df8749012093d572008fe2cc0a
hani-ibrahim <hani.eloksh@zalando.de>
2016-10-18 10:43:54 +0200

    #432: Adding payment

--- AtlasSDK/AtlasSDK/Models/Config.swift
+++ AtlasSDK/AtlasSDK/Models/Config.swift
@@ -11,6 +11,7 @@ public struct Config {
     public let loginURL: NSURL
     public let clientId: String
     public let salesChannel: String
+    public let payment: Payment
 
     public let salesChannelLocale: NSLocale
     public let interfaceLocale: NSLocale
@@ -21,6 +22,10 @@ public struct Config {
 
     public let tocURL: NSURL
 
+    public struct Payment {
+        let selectionCallbackURL: NSURL
+        let thirdPartyCallbackURL: NSURL
+    }
 }
 
 private typealias LocaleSalesChannel = (salesChannel: String, locale: String, tocURL: NSURL)
@@ -29,9 +34,11 @@ extension Config {
 
     init?(json: JSON, options: Options) {
         guard let
-        catalogURL = json["atlas-catalog-api"]["url"].URL,
+            catalogURL = json["atlas-catalog-api"]["url"].URL,
             checkoutURL = json["atlas-checkout-api"]["url"].URL,
             loginURL = json["oauth2-provider"]["url"].URL,
+            selectionCallbackURL = json["atlas-checkout-api"]["payment"]["selection-callback"].URL,
+            thirdPartyCallbackURL = json["atlas-checkout-api"]["payment"]["third-party-callback"].URL,
             localeSalesChannel = Config.findSalesChannel(json, channelId: options.salesChannel)
         else { return nil }
 
@@ -39,6 +46,7 @@ extension Config {
         self.checkoutURL = checkoutURL
         self.loginURL = loginURL
 
+        self.payment = Payment(selectionCallbackURL: selectionCallbackURL, thirdPartyCallbackURL: thirdPartyCallbackURL)
         self.salesChannel = localeSalesChannel.salesChannel
         self.salesChannelLocale = NSLocale(localeIdentifier: localeSalesChannel.locale)
         self.tocURL = localeSalesChannel.tocURL
