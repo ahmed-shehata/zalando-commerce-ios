c5b7a22ddf3eaa9e932d8b66c188feba150ff771
Daniel Bauke <daniel.bauke@zalando.de>
2016-10-26 17:24:40 +0200

    Add comments to postclone script

--- AtlasDemo/buddybuild_postclone.sh
+++ AtlasDemo/buddybuild_postclone.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+# TODO:
+# 1. merge from master
+# 2. replace sed on swift source to sed on Info.plist file
+
 sed -i ''  's#NSProcessInfo.*"USE_MOCK_API".*#true#' AtlasDemo/AppSetup.swift
