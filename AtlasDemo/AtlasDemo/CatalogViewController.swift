e79148e6f59af3b872e10cbcca78d2fe5f305ab8
hani-ibrahim <hani.eloksh@zalando.de>
2016-10-27 15:36:45 +0200

    cleaning up SKUs

--- AtlasDemo/AtlasDemo/CatalogViewController.swift
+++ AtlasDemo/AtlasDemo/CatalogViewController.swift
@@ -20,13 +20,9 @@ class CatalogViewController: UIViewController, UICollectionViewDelegate, UIColle
 
     private let articlesClient = ArticlesClient()
     private let sampleSKUs = [
-        "L2711E002-Q11", "AZ711N007-Q11", "AZ711N008-G11",
-        "GU121D08Z-Q11", "AZ711N007-Q11", "MA322E020-E11",
-        "AZ711M001-B11", "AZ711N00B-Q11", "MK151F00E-Q11",
-        "AR322O01B-I11", "M0Q21C068-B11", "UG151B002-C11",
-        "EV451D00U-302", "TP822C00N-Q11", "RA252F005-802",
-        "AJ152H012-Q11", "6CA51L00B-D11", "C1551E00K-F11",
-        "PS752G001-Q11", "PX652G005-K11"
+        "L2711E002-Q11", "GU121D08Z-Q11", "AZ711M001-B11",
+        "AZ711N00B-Q11", "MK151F00E-Q11", "M0Q21C068-B11",
+        "EV451D00U-302", "RA252F005-802"
     ]
 
     override func viewDidLoad() {
@@ -45,7 +41,7 @@ class CatalogViewController: UIViewController, UICollectionViewDelegate, UIColle
         articlesClient.fetch(articlesForSKUs: sampleSKUs) { result in
             switch result {
             case .success(let articles):
-                self.articles = articles
+                self.articles = articles.sort { $0.id < $1.id }
             case .failure(let error):
                 self.showError(title: "Error", error: error)
             }
