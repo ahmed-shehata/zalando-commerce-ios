4752ac2bed1aad5615571951cacb20701944d208
Daniel Bauke <daniel.bauke@zalando.de>
2016-10-31 13:05:14 +0100

    #498: Refactored Config.salesChannel to separated struct

--- AtlasSDK/AtlasSDK/Models/Config.swift
+++ AtlasSDK/AtlasSDK/Models/Config.swift
@@ -10,90 +10,108 @@ public struct Config {
     public let checkoutURL: NSURL
     public let loginURL: NSURL
     public let clientId: String
-    public let salesChannel: String
     public let payment: Payment
+    public let salesChannel: SalesChannel
+    public let availableSalesChannels: [SalesChannel]
 
-    public let salesChannelLocale: NSLocale
     public let interfaceLocale: NSLocale
-
-    public var countryCode: String {
-        return interfaceLocale.validCountryCode()
-    }
-
-    public let tocURL: NSURL
 
     public struct Payment {
         public let selectionCallbackURL: NSURL
         public let thirdPartyCallbackURL: NSURL
     }
-}
 
-private typealias LocaleSalesChannel = (salesChannel: String, locale: String, tocURL: NSURL)
+    public struct SalesChannel {
+        public let identifier: String
+        public let locale: NSLocale
+        public let termsAndConditionsURL: NSURL
+
+        public var countryCode: String {
+            return locale.validCountryCode()
+        }
+    }
+
+}
 
 extension Config {
 
     init?(json: JSON, options: Options) {
         guard let
-            catalogURL = json["atlas-catalog-api"]["url"].URL,
+        catalogURL = json["atlas-catalog-api"]["url"].URL,
             checkoutURL = json["atlas-checkout-api"]["url"].URL,
             loginURL = json["oauth2-provider"]["url"].URL,
             selectionCallbackURL = json["atlas-checkout-api"]["payment"]["selection-callback"].URL,
             thirdPartyCallbackURL = json["atlas-checkout-api"]["payment"]["third-party-callback"].URL,
-            localeSalesChannel = Config.findSalesChannel(json, channelId: options.salesChannel)
-        else { return nil }
+            availableSalesChannels = json["sales-channels"].array?.flatMap({ SalesChannel(json: $0) }),
+            salesChannel = availableSalesChannels.filter({ $0.identifier == options.salesChannel}).first
+            else { return nil }
 
         self.catalogURL = catalogURL
         self.checkoutURL = checkoutURL
         self.loginURL = loginURL
+        self.availableSalesChannels = availableSalesChannels
 
         self.payment = Payment(selectionCallbackURL: selectionCallbackURL, thirdPartyCallbackURL: thirdPartyCallbackURL)
-        self.salesChannel = localeSalesChannel.salesChannel
-        self.salesChannelLocale = NSLocale(localeIdentifier: localeSalesChannel.locale)
-        self.tocURL = localeSalesChannel.tocURL
+        self.salesChannel = salesChannel
         if let interfaceLanguage = options.interfaceLanguage {
-            self.interfaceLocale = NSLocale(localeIdentifier: "\(interfaceLanguage)_\(salesChannelLocale.validCountryCode())")
+            self.interfaceLocale = NSLocale(localeIdentifier: "\(interfaceLanguage)_\(salesChannel.countryCode)")
         } else {
-            self.interfaceLocale = salesChannelLocale
+            self.interfaceLocale = salesChannel.locale
         }
 
         self.clientId = options.clientId
     }
 
-    private static func findSalesChannel(json: JSON, channelId: String) -> LocaleSalesChannel? {
-        guard let matchedChannel = firstMatchedChannel(json, channelId: channelId),
-            salesChannel = matchedChannel["sales-channel"]?.string,
-            locale = matchedChannel["locale"]?.string,
-            tocURL = matchedChannel["toc_url"]?.URL
-        else {
-            return nil
-        }
-
-        return (salesChannel: salesChannel, locale: locale, tocURL: tocURL)
-    }
-
-    private static func firstMatchedChannel(json: JSON, channelId: String) -> [String: JSON]? {
-        guard let availableChannels = json["sales-channels"].array else {
-            return nil
-        }
-        let channel = availableChannels.filter { channel in
-            return channel["sales-channel"].string?.lowercaseString.containsString(channelId) ?? false
-        }.first
-
-        return channel?.dictionary
-    }
-
 }
 
 extension Config: CustomStringConvertible {
+
     public var description: String {
         return "Config: { catalogURL: \(self.catalogURL)"
             + ", checkoutURL: \(self.checkoutURL)"
             + ", loginURL: \(self.loginURL)"
-            + ", salesChannel: \(self.salesChannel)"
             + ", clientId: \(self.clientId)"
-            + ", salesChannelLocale: \(self.salesChannelLocale.localeIdentifier)"
+            + ", salesChannel: \(self.salesChannel)"
+            + ", payment: \(self.payment)"
             + ", interfaceLocale: \(self.interfaceLocale.localeIdentifier)"
-            + ", tocURL: \(self.tocURL)"
             + " }"
     }
+
+}
+
+extension Config.SalesChannel: JSONInitializable {
+
+    init?(json: JSON) {
+        guard let identifier = json["sales-channel"].string,
+            localeIdentifier = json["locale"].string,
+            tocURL = json["toc_url"].URL
+            else { return nil }
+
+        self.identifier = identifier
+        self.locale = NSLocale(localeIdentifier: localeIdentifier)
+        self.termsAndConditionsURL = tocURL
+    }
+
+}
+
+extension Config.SalesChannel: CustomStringConvertible {
+
+    public var description: String {
+        return "SalesChannel: { identifier: \(self.identifier)"
+            + ", locale: \(self.locale.localeIdentifier)"
+            + ", termsAndConditionsURL: \(self.termsAndConditionsURL)"
+            + ", countryCode: \(self.countryCode)"
+            + " }"
+    }
+
+}
+
+extension Config.Payment: CustomStringConvertible {
+
+    public var description: String {
+        return "Payment: { selectionCallbackURL: \(self.selectionCallbackURL)"
+            + ", thirdPartyCallbackURL: \(self.thirdPartyCallbackURL)"
+            + " }"
+    }
+
 }
