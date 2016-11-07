c4418a830929cc3c63a3ea1a1d03ab5fc57b2e31
Daniel Bauke <daniel.bauke@zalando.de>
2016-11-07 15:54:36 +0100

    Fixed path in buddybuild_postclone.sh

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -4,4 +4,4 @@
 # 1. merge from master
 # 2. replace sed on swift source to sed on Info.plist file
 
-sed -i ''  's#NSProcessInfo.*"USE_MOCK_API".*#true#' AtlasDemo/AppSetup.swift
+sed -i ''  's#NSProcessInfo.*"USE_MOCK_API".*#true#' AtlasDemo/AtlasDemo/AppSetup.swift
