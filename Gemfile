610e0ec5685e2d45307999eee41efc8d23cd3335
Daniel Bauke <daniel.bauke@zalando.de>
2016-11-09 13:01:18 +0100

    #321: Auto commit version update in plist files

--- Gemfile
+++ Gemfile
@@ -3,16 +3,17 @@ source 'https://rubygems.org'
 ruby 2.2
 
 group :calypso do
-  gem 'thor'
-  gem 'xcpretty'
-  gem 'github_api'
-  gem 'daemons'
-  gem 'xcov'
-  gem 'jazzy'
-  gem 'github_changelog_generator'
-  gem 'rubocop'
-  gem 'httparty'
   gem 'awesome_print'
+  gem 'daemons'
+  gem 'git'
+  gem 'github_api'
+  gem 'github_changelog_generator'
+  gem 'httparty'
+  gem 'jazzy'
+  gem 'rubocop'
+  gem 'thor'
+  gem 'xcov'
+  gem 'xcpretty'
 end
 
 group :tools do
--- lib/calypso/release.rb
+++ lib/calypso/release.rb
@@ -1,4 +1,5 @@
 require 'thor'
+require 'git'
 require_relative 'run'
 
 VERSION_FILE = File.expand_path('../../version.rb', __FILE__)
@@ -10,14 +11,13 @@ module Calypso
 
     desc 'create_version', 'Creates new version: updates plist files, add a tag and push to the GitHub'
     def create_version
-      abort('Please commit all changes before creating new version') unless `git status --porcelain`.empty?
+      abort('Please commit all changes before creating new version') if repo_changes?
 
       new_version = ask("Enter new version (current #{ATLAS_VERSION}):", :blue)
       new_version = ATLAS_VERSION if new_version.empty?
       write_new_version(new_version)
 
-      update_plist_version(new_version: new_version, plist: 'AtlasSDK/AtlasSDK/Info.plist')
-      update_plist_version(new_version: new_version, plist: 'AtlasUI/AtlasUI/Info.plist')
+      update_plist_version(new_version, 'AtlasSDK/AtlasSDK/Info.plist', 'AtlasUI/AtlasUI/Info.plist')
 
       say 'Execute the following commands:'
       say "  git tag '#{new_version}'\n"\
@@ -28,12 +28,25 @@ module Calypso
 
     include Run
 
+    def repo
+      @repo ||= Git.open(File.expand_path('../../..', __FILE__))
+    end
+
     def write_new_version(new_version)
       File.open(VERSION_FILE, 'w') { |file| file.puts("ATLAS_VERSION = '#{new_version}'") }
     end
 
-    def update_plist_version(new_version:, plist:)
-      run "/usr/libexec/PlistBuddy -c 'Set :CFBundleShortVersionString #{new_version}' '#{plist}'"
+    def update_plist_version(new_version, *args)
+      args.each do |plist|
+        run "/usr/libexec/PlistBuddy -c 'Set :CFBundleShortVersionString #{new_version}' '#{plist}'"
+        repo.add plist
+      end
+      repo.commit "[auto] Updated plist version to #{new_version} h"
+    end
+
+    def repo_changes?
+      changed = repo.status.map(&:type).compact
+      changed.count.nonzero?
     end
 
   end
