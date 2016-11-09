84ab60b1ce31d6066abb3254d56a3d3a0b25bcca
Daniel Bauke <daniel.bauke@zalando.de>
2016-11-09 13:24:13 +0100

    #321: Refactor version commits

--- lib/calypso/release.rb
+++ lib/calypso/release.rb
@@ -9,19 +9,29 @@ module Calypso
 
   class Release < Thor
 
+    option :tag, type: :boolean
+    option :push, type: :boolean
     desc 'create_version', 'Creates new version: updates plist files, add a tag and push to the GitHub'
     def create_version
-      abort('Please commit all changes before creating new version') if repo_changes?
+      if repo_changes?
+        say 'Please commit all changes before creating new version', :red
+        abort
+      end
 
       new_version = ask("Enter new version (current #{ATLAS_VERSION}):", :blue)
       new_version = ATLAS_VERSION if new_version.empty?
-      write_new_version(new_version)
 
-      update_plist_version(new_version, 'AtlasSDK/AtlasSDK/Info.plist', 'AtlasUI/AtlasUI/Info.plist')
+      update_plist(new_version, 'AtlasSDK/AtlasSDK/Info.plist')
+      update_plist(new_version, 'AtlasUI/AtlasUI/Info.plist')
+      update_version_file(new_version)
+      commit_version(new_version)
 
-      say 'Execute the following commands:'
-      say "  git tag '#{new_version}'\n"\
-        '  git push --tags', :yellow
+      if options[:tag] || yes?("Would you like to tag current commit with #{new_version}?", :red)
+        say "  git tag '#{new_version}'", :yellow
+      end
+      if options[:push] || yes?('Would you like to push changes?', :red)
+        say '  git push --tags', :yellow
+      end
     end
 
     private
@@ -32,16 +42,18 @@ module Calypso
       @repo ||= Git.open(File.expand_path('../../..', __FILE__))
     end
 
-    def write_new_version(new_version)
+    def update_version_file(new_version)
       File.open(VERSION_FILE, 'w') { |file| file.puts("ATLAS_VERSION = '#{new_version}'") }
+      repo.add VERSION_FILE
     end
 
-    def update_plist_version(new_version, *args)
-      args.each do |plist|
-        run "/usr/libexec/PlistBuddy -c 'Set :CFBundleShortVersionString #{new_version}' '#{plist}'"
-        repo.add plist
-      end
-      repo.commit "[auto] Updated plist version to #{new_version} h"
+    def update_plist(new_version, plist)
+      run "/usr/libexec/PlistBuddy -c 'Set :CFBundleShortVersionString #{new_version}' '#{plist}'"
+      repo.add plist
+    end
+
+    def commit_version(new_version)
+      repo.commit "[auto] Updated version to #{new_version}"
     end
 
     def repo_changes?
