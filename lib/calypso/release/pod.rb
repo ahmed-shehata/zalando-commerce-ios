9ecfc55b7ec6acf518d37c4a22e306f9fbeb9cf8
Daniel Bauke <daniel.bauke@zalando.de>
2016-11-30 13:41:28 +0100

    Extended calypso pod with more options

--- lib/calypso/release/pod.rb
+++ lib/calypso/release/pod.rb
@@ -7,27 +7,34 @@ module Calypso
 
     option :local, type: :boolean
     option :silent, type: :boolean
+    option :verbose, type: :boolean
     desc 'validate', 'Validates and builds pod'
     def validate
       if options[:local]
-        run_pod 'lib lint', options[:silent]
+        run_pod 'lib lint', options
       else
-        run_pod 'spec lint', options[:silent]
+        run_pod 'spec lint', options
       end
     end
 
     option :silent, type: :boolean
     desc 'publish', 'Publish new version to CocoaPods'
     def publish
-      run_pod 'trunk push', options[:silent]
+      run_pod 'trunk push', options
     end
 
     private
 
     include Run
 
-    def run_pod(subcommand, silent)
-      run "pod #{subcommand} AtlasSDK.podspec --allow-warnings #{silent ? '--silent' : ''}"
+    def run_pod(subcommand, options)
+      verbosity_arg = if options[:silent]
+                        '--silent'
+                      elsif options[:verbose]
+                        '--verbose'
+                      end
+
+      run "pod #{subcommand} AtlasSDK.podspec --allow-warnings #{verbosity_arg}"
     end
 
   end
