e9a610a3c2b8cb1298d47ce3fc6fa7e0394425dd
hani-ibrahim <hani.eloksh@zalando.de>
2016-11-30 10:28:36 +0100

    fixing tests

--- AtlasMockAPI/AtlasMockAPI/Endpoints/guestOrder.swift
+++ AtlasMockAPI/AtlasMockAPI/Endpoints/guestOrder.swift
@@ -18,7 +18,9 @@ extension HttpServer {
                 let url = "https://payment-gateway.kohle-integration.zalan.do/payment-method-selection-session/TOKEN/selection"
                 return HttpResponse.RAW(204, "No Content", ["Location": url], nil)
             } else {
-                return .MovedPermanently("http://localhost.charlesproxy.com:9080/guest-checkout/api/orders/withPaymentInRequest")
+                // swiftlint:disable:next line_length
+                let json = "{\"order_number\":\"10105083300694\",\"billing_address\":{\"gender\":\"MALE\",\"first_name\":\"John\",\"last_name\":\"Doe\",\"street\":\"Mollstr. 1\",\"zip\":\"10178\",\"city\":\"Berlin\",\"country_code\":\"DE\"},\"shipping_address\":{\"gender\":\"MALE\",\"first_name\":\"John\",\"last_name\":\"Doe\",\"street\":\"Mollstr. 1\",\"zip\":\"10178\",\"city\":\"Berlin\",\"country_code\":\"DE\"},\"gross_total\":{\"amount\":10.45,\"currency\":\"EUR\"},\"tax_total\":{\"amount\":2.34,\"currency\":\"EUR\"},\"created\":\"2016-11-29T14:14:25.126Z\",\"detail_url\":\"https://www-de.release.zalando.net/benutzerkonto/bestellung-detail/10105083300694\"}"
+                return .OK(.Text(json))
             }
         }
     }
