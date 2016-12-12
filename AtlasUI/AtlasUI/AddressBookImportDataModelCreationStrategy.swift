c9dfd124a7947c7076003245c655d459eec7bf8b
Hani <Hani@Hanis-MacBook-Air.local>
2016-12-12 10:01:48 +0100

    check on the address country code

--- AtlasUI/AtlasUI/AddressBookImportDataModelCreationStrategy.swift
+++ AtlasUI/AtlasUI/AddressBookImportDataModelCreationStrategy.swift
@@ -31,6 +31,13 @@ extension AddressBookImportDataModelCreationStrategy: CNContactPickerDelegate {
     func contactPicker(_ picker: CNContactPickerViewController, didSelect contactProperty: CNContactProperty) {
         picker.dismiss(animated: true) { [weak self] in
             guard let strongSelf = self else { return }
+            guard let postalAddress = contactProperty.value as? CNPostalAddress,
+                postalAddress.isoCountryCode.lowercased() == AtlasAPIClient.shared?.salesChannelCountry.lowercased()
+                else {
+                    UserMessage.displayError(error: AtlasCheckoutError.unsupportedCountry)
+                    return
+            }
+
             if let datawModel = AddressFormDataModel(contactProperty: contactProperty,
                                                      countryCode: AtlasAPIClient.shared?.salesChannelCountry) {
                 strongSelf.completion(datawModel)
