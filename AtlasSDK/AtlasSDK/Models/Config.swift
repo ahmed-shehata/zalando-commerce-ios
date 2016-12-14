e6fe31a283a0aec020d74fd70cb4ce7291885be2
hani-ibrahim <hani.eloksh@zalando.de>
2016-12-14 17:29:17 +0100

    add isGuestCheckoutEnabled flag

--- AtlasSDK/AtlasSDK/Models/Config.swift
+++ AtlasSDK/AtlasSDK/Models/Config.swift
@@ -13,6 +13,7 @@ public struct Config {
 
     public let clientId: String
     public let useSandboxEnvironment: Bool
+    public let guestCheckoutEnabled: Bool
 
     public let payment: Payment
     public let salesChannel: SalesChannel
@@ -71,6 +72,7 @@ extension Config {
 
         self.clientId = options.clientId
         self.useSandboxEnvironment = options.useSandboxEnvironment
+        self.guestCheckoutEnabled = json["atlas-guest-checkout-api"]["enabled"].bool ?? false
     }
 
 }
