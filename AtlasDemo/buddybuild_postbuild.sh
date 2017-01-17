664e5d27fa5284e49656b0852faf29d64fa1e6f9
Daniel Bauke <daniel.bauke@zalando.de>
2017-01-17 15:35:33 +0100

    [buddybuild] Turned off testdroid in postbuild

--- AtlasDemo/buddybuild_postbuild.sh
+++ AtlasDemo/buddybuild_postbuild.sh
@@ -4,9 +4,11 @@ cd ..
 
 git describe --tags --exact-match > /dev/null 2> /dev/null
 if [[ $? -eq 0 ]]; then
+	echo Verifying pods
 	pod repo update 2> /dev/null
 	pod lib lint AtlasSDK.podspec --allow-warnings
 	pod lib lint AtlasUI.podspec --allow-warnings
 fi
-cd AtlasDemo
-./testdroid_upload.sh
+
+# cd AtlasDemo
+# ./testdroid_upload.sh
