5161b607f5af3b3452e1a4d9f77e2cb0a8286bb8
Daniel Bauke <daniel.bauke@zalando.de>
2017-01-24 12:51:21 +0100

    #693: Moved tests to JSON folder, added array and string tests

---
+++ AtlasSDK/AtlasSDKTests/JSON/JSONDictionariesTests.swift
@@ -0,0 +1,16 @@
+//
+//   Copyright © 2017 Zalando SE. All rights reserved.
+//
+
+import XCTest
+import Nimble
+
+@testable import AtlasSDK
+
+class JSONDictionariesTests: JSONTestCase {
+
+    func testDictionary() {
+    }
+
+}
+
