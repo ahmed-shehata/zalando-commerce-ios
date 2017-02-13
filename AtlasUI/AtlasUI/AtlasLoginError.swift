7b92d0b2213f17f67aebb63847cc861df8bda93e
Daniel Bauke <daniel.bauke@zalando.de>
2017-02-13 12:43:37 +0100

    #33: Added docs to AtlasLoginError and made it internal

--- AtlasUI/AtlasUI/AtlasLoginError.swift
+++ AtlasUI/AtlasUI/AtlasLoginError.swift
@@ -5,12 +5,27 @@
 import Foundation
 import AtlasSDK
 
-public enum AtlasLoginError: AtlasError {
+/// Error thrown in case of login failures.
+///
+/// The message is translated and presented inside login controller
+///
+/// - seealso: `OAuth2LoginViewController`
+///
+/// - accessDenied: Regular case. Could happen in 2 cases:
+///     - when a user doesn't give a consent
+///     - when a token is invalid / expired
+/// - requestFailed: Regular case, when connection problem occured.
+/// - missingURL: Fatal unexpected case, when webview didn't have a URL to load.
+///   Should never happen, but UIWebViewDelegate requires to handle this case.
+/// - missingViewControllerToShowLoginForm: Fatal unexpected case, when there's
+///   no controller from which a login controller could be presented for a user.
+///   Should never happen, it's a grace exit.
+enum AtlasLoginError: AtlasError {
+
+    case accessDenied
+    case requestFailed(error: Error?)
 
     case missingURL
-    case accessDenied
     case missingViewControllerToShowLoginForm
-
-    case requestFailed(error: Error?)
 
 }
